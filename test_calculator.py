0b9c110bb2335ed4dbe2562a549cba9808acbbe4
zenith9225 <zenith9225@gmail.com>
2026-04-08 15:45:38 -0400

    final merge

--- test_calculator.py
+++ test_calculator.py
@@ -1,3 +1,7 @@
+# https://github.com/zenith9225/lab11-rc-zp.git
+# Partner 1: Zack Phillips
+# Partner 2: Riley Carr
+
 import unittest
 from calculator import *
 
@@ -11,10 +15,18 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
+    def test_multiply(self): # 3 assertions
+        self.assertAlmostEqual(mul(2,3),6)
+        self.assertAlmostEqual(mul(-2,3),-6)
+        self.assertAlmostEqual(mul(3,0),0)
+        self.assertAlmostEqual(mul(-3,-2),6)
     #     fill in code
 
-    # def test_divide(self): # 3 assertions
+    def test_divide(self): # 3 assertions
+        self.assertAlmostEqual(div(3,6),2)
+        self.assertAlmostEqual(div(-3,6),-2)
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 3)
     #     fill in code
     # ##########################
 
@@ -34,16 +46,27 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithm(8, 0)
+        with self.assertRaises(ValueError):
+            logarithm(8, -2)
     #     # call log function inside, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     logarithm(0, 5)
     #     fill in code
 
-    # def test_hypotenuse(self): # 3 assertions
+    def test_hypotenuse(self): # 3 assertions
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(-3, -4), 5)
+        self.assertAlmostEqual(hypotenuse(5, -12), 13)
     #     fill in code
 
-    # def test_sqrt(self): # 3 assertions
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+            square_root(-1)
+        self.assertAlmostEqual(square_root(0), 0)
+        self.assertAlmostEqual(square_root(4), 2)
     #     # Test for invalid argument, example:
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #    square_root(NUM)
